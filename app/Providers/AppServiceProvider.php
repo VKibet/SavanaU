f6ccc7b3242b7c71507dc78b47552d2199938b10
Avick <victordakibet@gmail.com>
2026-04-21 12:47:46 +0300

    AppSettings2

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,7 +2,6 @@
 
 namespace App\Providers;
 
-use Illuminate\Support\Facades\Vite;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -20,9 +19,6 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        // Use compiled manifest if it exists (production builds)
-        if (file_exists(public_path('build/manifest.json'))) {
-            Vite::useManifest(public_path('build/manifest.json'));
-        }
+        //
     }
 }
